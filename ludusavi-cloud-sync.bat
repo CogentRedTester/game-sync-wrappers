551dcb688fb064021e80ebb0e426b9625b15bd25
CogentRedTester <cogent.redtester@outlook.com>
2026-04-10 11:10:05 +0930

    notify when cloud sync failed

--- ludusavi-cloud-sync.bat
+++ ludusavi-cloud-sync.bat
@@ -1,9 +1,24 @@
 @echo off
+
+REM Show GUI and commandline error messages
+:err
+    setlocal enabledelayedexpansion
+        set "msg=%~1"
+
+        >&2 echo !msg!
+
+        powershell -NoProfile -Command ^
+            "Add-Type -AssemblyName System.Windows.Forms; ^
+            [System.Windows.Forms.MessageBox]::Show('!msg!','ludusavi-cloud-sync.ps1','OK','Error')"
+
+    endlocal
+exit /b 0
+
+
 REM Check if Ludusavi is available
 where ludusavi >nul 2>&1
 if %ERRORLEVEL% NEQ 0 (
-    echo Ludusavi is not installed or not in PATH.
-    powershell -NoProfile -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Ludusavi not found, aborting script.','ludusavi-cloud-sync.ps1',[System.Windows.Forms.MessageBoxButtons]::OK,[System.Windows.Forms.MessageBoxIcon]::Error)"
+    call:err "Ludusavi not found, aborting script."
     exit /b 1
 ) else (
     echo Using system Ludusavi
@@ -16,10 +31,10 @@ REM Taken from https://stackoverflow.com/a/761658
 set "game_exe=%~2"
 set game_args=%3
 :loop
-shift
-if [%3]==[] goto afterloop
-set game_args=%game_args% %3
-goto loop
+    shift
+    if [%3]==[] goto afterloop
+    set game_args=%game_args% %3
+    goto loop
 :afterloop
 
 REM Uses PowerShell to get Ludusavi backup path
@@ -29,8 +44,16 @@ for /f "usebackq delims=" %%B in (`powershell -NoProfile -Command ^
 REM # Create a local backup of the game before restoring files from the cloud to provide a recovery option in case of save conflicts
 ludusavi backup --path "%ludusavi_backup_dir%/.backup" --full-limit 2 --force --no-cloud-sync "%ludusavi_game%"
 
-REM Overwrite local saves with cloud saves and restore the latest save file
+REM Overwrite local saves with cloud saves
 ludusavi cloud download --force "%ludusavi_game%"
+if ERRORLEVEL EQU 0 (
+    set cloud_sync=0
+) else (
+    set cloud_sync=1
+    call:err "Failed to download save from the cloud, will not attempt upload after game closes."
+)
+
+REM Restore the latest save file
 ludusavi restore --force --gui --ask-downgrade --no-cloud-sync  "%ludusavi_game%"
 
 REM Run the game
@@ -38,4 +61,7 @@ REM Run the game
 
 REM Back up the game and overwrite the cloud saves with the local saves
 ludusavi backup --force --gui --no-cloud-sync  "%ludusavi_game%"
-ludusavi cloud upload --force "%ludusavi_game%"
+
+if %cloud_sync% EQU 0 (
+    ludusavi cloud upload --force "%ludusavi_game%"
+)
--- ludusavi-cloud-sync.sh
+++ ludusavi-cloud-sync.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Show GUI and commandline error messages
+err() {
+    echo "$1" >&2
+
+    if command -v kdialog >/dev/null 2>&1; then
+        kdialog --title "ludasavi-cloud-sync.sh" --error "$1"
+    elif command -v zenity >/dev/null 2>&1; then
+        zenity --error --text="$1"
+    elif command -v notify-send >/dev/null 2>&1; then
+        notify-send -w "Error" "$1"
+    fi
+}
+
 # Check if ludusavi is available
 if command -v ludusavi >/dev/null 2>&1; then
     echo 'Using system Ludusavi'
@@ -12,35 +25,34 @@ elif flatpak run com.github.mtkennerly.ludusavi -V >/dev/null 2>&1; then
     }
 
 else
-    echo "Ludusavi not found, aborting script."
-
-    # Show GUI error messages
-    if command -v kdialog >/dev/null 2>&1; then
-        kdialog --title "ludasavi-cloud-sync.sh" --error "Ludusavi not found, aborting script."
-    elif command -v zenity >/dev/null 2>&1; then
-        zenity --error --text="Ludusavi not found, aborting script."
-    elif command -v notify-send >/dev/null 2>&1; then
-        notify-send -w "Error" "Ludusavi not found, aborting script."
-    fi
-
+    err "Ludusavi not found, aborting script."
     exit 1
 fi
 
 ludusavi_game="$1"
 ludusavi_backup_dir=$(ludusavi config show --api | jq -r '.backup.path')
 
-shift
-
 # Create a local backup of the game before restoring files from the cloud to provide a recovery option in case of save conflicts
 ludusavi backup --path "${ludusavi_backup_dir}/.backup" --full-limit 2 --force --no-cloud-sync "$ludusavi_game"
 
-# Overwrite local saves with cloud saves and restore the latest save file
-ludusavi cloud download --force "$ludusavi_game"
+# Overwrite local saves with cloud saves
+if ludusavi cloud download --force "$ludusavi_game"; then
+    cloud_sync=0
+else
+    err "Failed to download save from the cloud, will not attempt upload after game closes."
+    cloud_sync=1
+fi
+
+# Restore the latest save file
 ludusavi restore --force --gui --ask-downgrade --no-cloud-sync "$ludusavi_game"
 
 # Run the game
+shift
 "$@"
 
 # Back up the game and overwrite the cloud saves with the local saves
 ludusavi backup --force --gui --no-cloud-sync "$ludusavi_game"
-ludusavi cloud upload --force "$ludusavi_game"
+
+if [ "$cloud_sync" -eq 0 ]; then
+    ludusavi cloud upload --force "$ludusavi_game"
+fi
